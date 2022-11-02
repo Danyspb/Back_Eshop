eec07f221351203367487baec599366518c1f758
Danyspb4ever <danyspb4ever@gmail.com>
2022-11-02 22:49:33 +0000

    user started

--- models/user.js
+++ models/user.js
@@ -1,15 +1,57 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name:{
         type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
         unique: true,
     },
-    image: String,
-    quantity:{
-        type: Number,
-        required: true
+    passwordHash:{
+        type: String,
+        require: true
+    },
+    phone:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartement: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 })
 
-exports.Product = mongoose.model('User', productSchema)
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+userSchema.set('toJSON',{
+    virtuals: true,
+});
+
+exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
--- routers/usersR.js
+++ routers/usersR.js
@@ -1,9 +1,10 @@
 const {User} = require('../models/user');
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const routers = express.Router();
 
 
-
+///// get all users //////
 routers.get(`/`, async(req,res)=>{
     const userList = await User.find();
 
@@ -13,6 +14,28 @@ routers.get(`/`, async(req,res)=>{
 
     res.send(userList)
 
+})
+
+
+routers.post('/', async(req, res)=>{
+    let creatuser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.passwordHash, 10),
+        phone: req.body.phone,
+        street:req.body.street,
+        apartement: req.body.apartement,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        isAdmin: req.body.isAdmin
+    }) 
+    creatuser = await creatuser.save();
+    if(!creatuser){
+        return res.status(500).json({succes: false, message: 'utilisateur non cree !!!'})
+    }else{
+        return res.status(201).json({succes: true, message: 'utilisateur cree avec succes !!!'})
+    }
 })
 
 module.exports = routers;
