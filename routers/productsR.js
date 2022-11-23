2c4d9b4c5e85a8589d859d39f69536d9aaa263dc
Danyspb4ever <danyspb4ever@gmail.com>
2022-11-23 17:38:47 +0000

    upload file

--- routers/productsR.js
+++ routers/productsR.js
@@ -6,13 +6,25 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg'
+}
+
 const storage = multer.diskStorage({
    destination: function(req, file, cb){
-      cb(null , 'public/upload')
+      const isValideFile = FILE_TYPE_MAP[file.mimetype];
+      let upLoaderError = new Error('invalid image type');
+      if(isValideFile){
+        upLoaderError = null
+      }
+      cb(upLoaderError , 'public/upload')
    },
    filename: function (req, file, cb){
-      const fileName = file.originalname.split(' ').join('-');
-      cb(null, `${fileName}-{Date.now()}.${extension}`)
+      const fileName = file.originalname.replace(/(.png|.jpeg|.jpg)/,'')
+      const extension = FILE_TYPE_MAP[file.mimetype];
+      cb(null, `${fileName}-${Date.now()}.${extension}`)
    }
 })
 
